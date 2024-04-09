7430f557217940265a9ede1694d4e283041f9b84
stehu-ai <stehu19@gmail.com>
2024-04-09 16:50:12 +0300

    Injected the forecast html from javascript, added some css, updated the html to a more friendlier outlook

--- src/index.js
+++ src/index.js
@@ -40,7 +40,30 @@ function handleSearchSubmit(event) {
     
     searchCity(searchInput.value);
 }
+
+function displayForecast () {
+   let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+   let forecastHtml = "";
+
+   days.forEach(function(day) {
+    forecastHtml = forecastHtml +
+    `<div class="weather-forecast-day">
+                    <div class="weather-forecast-date">${day}</div>
+                    <div class="weather-forecast-icon">⛅</div>
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature">
+                            <strong>18°</strong>
+                        </span>
+                        <span class="weather-forecast-temperature">
+                            12°</span>
+                    </div>
+                </div>`; 
+   });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
