e4762b21b1a0c33951bf794b25d9adcfb3abe388
stehu-ai <stehu19@gmail.com>
2024-04-10 12:33:20 +0300

    Changed the title document, added some style to the css file, added timestamp to create days of the week.

--- src/index.js
+++ src/index.js
@@ -43,6 +43,13 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri","Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "ba3370o045f71d40a584b42abf8btef1";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -50,22 +57,23 @@ function getForecast(city) {
 }
 function displayForecast (response) {
     
-   let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
    let forecastHtml = "";
 
-   days.forEach(function(day) {
+   response.data.daily.forEach(function(day, index) {
+    if (index < 5) {
     forecastHtml = forecastHtml +
     `<div class="weather-forecast-day">
-                    <div class="weather-forecast-date">${day}</div>
-                    <div class="weather-forecast-icon">⛅</div>
+                    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                    <div class="weather-forecast-icon"> <img src="${day.condition.icon_url}" /> </div>
                     <div class="weather-forecast-temperatures">
                         <span class="weather-forecast-temperature">
-                            <strong>18°</strong>
+                            <strong>${Math.round(day.temperature.maximum)}°</strong>
                         </span>
                         <span class="weather-forecast-temperature">
-                            12°</span>
+                            ${Math.round(day.temperature.minimum)}°</span>
                     </div>
                 </div>`; 
+    }
    });
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
@@ -73,6 +81,6 @@ function displayForecast (response) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("London");
 
 
